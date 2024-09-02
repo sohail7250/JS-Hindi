64b98bb0a34c735f851609e8acc2375eda5bb7b9
sohail7250 <52400330+sohail7250@users.noreply.github.com>
2024-09-02 13:29:31 +0000

    function done

---
+++ 02_basics/05_functions.js
@@ -0,0 +1,72 @@
+
+// console.log("S");
+// console.log("O");
+// console.log("H");
+// console.log("A");
+// console.log("I");
+// console.log("L");
+
+// function sayMyName(){
+//      console.log("S");
+//      console.log("O");
+//      console.log("H");
+//      console.log("A");
+//      console.log("I");
+//      console.log("L");
+    
+// }
+
+//  sayMyName      // reference
+
+//   sayMyName()        // execute  
+
+//   function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+
+// }
+
+
+    //  addTwoNumbers(3, 5);
+
+    // addTwoNumbers(3,"5");
+
+    // addTwoNumbers(3, "a");
+
+    // addTwoNumbers(3, null);    // null 0
+
+    // const result = addTwoNumbers(3, 5)
+
+    // console.log("Result: ", result);
+    
+
+
+
+  function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // console.log("sohail");
+    // return result
+    // // console.log("sohail");
+
+    return number1 + number2
+    
+     }
+
+     const result = addTwoNumbers(3, 5)
+
+    //  console.log("Result: ", result)
+
+   function loginUserMessage(username  = "Rock"){
+    if(username === undefined){
+        console.log("please enter a username");
+
+        return
+    }
+
+    return `${username} just logged in`
+
+   }
+
+   console.log(loginUserMessage("sohail"))
+   console.log(loginUserMessage(""))
+   console.log(loginUserMessage())   // undefined
+
