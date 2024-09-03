68e5585c52c6f2fb75c28eb46862d66fd8205389
sohail7250 <52400330+sohail7250@users.noreply.github.com>
2024-09-03 15:24:16 +0000

    arrays2 done

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -11,7 +11,7 @@ const dc_heros = ["Batman", "Superman", "Flash"];
 // console.log(allHeros);
 
 
-// const all_new_heros = [...marvel_heros, ...dc_heros]  // spread method() can't return the result.
+// const all_new_heros = [...marvel_heros, ...dc_heros]  
 // console.log(all_new_heros);
 
 // const another_array = [1, 2, 3, [4, 5, 6], [ 6, 5]]
