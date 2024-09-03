936a2f79e899a7c850c7dc8bfbdfa17f37547435
sohail7250 <52400330+sohail7250@users.noreply.github.com>
2024-09-03 08:59:25 +0000

    scopes done

--- 02_basics/07_scopes.js
+++ 02_basics/07_scopes.js
@@ -7,12 +7,55 @@ if (true) {
     const b = 20
     var c = 30
 
-    console.log("INNER:", a);
+    // console.log("INNER:", a);
     
     
 }
 
+// console.log(a);
+// // console.log(b);
+// console.log(c);
 
-console.log(a);
-// console.log(b);
-console.log(c);
+
+// **************  Nested Scopes  *******************
+
+
+function one(){
+    const username = "sohail"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+
+    } 
+    // console.log(website);
+     two()
+    
+}
+
+// one()
+
+
+if (true) {
+    const username = "sohail"
+    if (username ==="sohail") {
+        const website = " youtube"
+        console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+
+// *******************  intresting concept ******************************
+
+function addone(num){
+    return num + 1
+}
+
+addone(5)
+
