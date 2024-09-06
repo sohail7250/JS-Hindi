c4d5784bb8e5cab76ef557c03f49413763897ed1
sohail7250 <52400330+sohail7250@users.noreply.github.com>
2024-09-06 15:11:29 +0000

    completed

--- 04_control_flow/04_loops.js
+++ 04_control_flow/04_loops.js
@@ -26,10 +26,36 @@ for (let i = 0; i <= 10; i++) {
 
 let myArray = ["flash", "batman", "superman"]
 
-console.log(myArray.length)
+// console.log(myArray.length)
 
-for (let index = 0; index < array.length; index++) {
+for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
-    console.log(element);
+    // console.log(element);
+}
+
+
+// break and continue
+
+// for (let i = 0; i < 20; i++) {
+//     if (i == 5) {
+
+//         console.log(`Detected 5`);
+//         // break;
+        
+//     }
+//  console.log(`value of i is ${i}`)
+    
+// }
+
+
+for (let i = 0; i <= 20; i++) {
+    if (i == 5) {
+
+        console.log(`Detected 5`);
+        continue;
+        
+    }
+ console.log(`value of i is ${i}`)
+    
 }
 
