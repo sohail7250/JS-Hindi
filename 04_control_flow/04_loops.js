b9a49c8287a34418fe36b2ec3286b90595f87d96
sohail7250 <52400330+sohail7250@users.noreply.github.com>
2024-09-06 14:58:37 +0000

    completed

---
+++ 04_control_flow/04_loops.js
@@ -0,0 +1,35 @@
+//  for loop
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 3) {
+        // console.log("3 is best number")
+    }
+    // console.log(element)
+    
+}
+
+// console.log(element)           //  error scope related issue
+
+
+
+
+// for (let i = 2; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//    for (let j = 2; j <= 10; j++) {
+// //    console.log(`Inner loop value ${j} and inner loop ${i}`);
+
+// console.log(i + ' * ' + j + ' = ' + i*j)
+    
+//    }
+// }
+
+let myArray = ["flash", "batman", "superman"]
+
+console.log(myArray.length)
+
+for (let index = 0; index < array.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
+
