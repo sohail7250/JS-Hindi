456e7b0d3d1048b7ba4737da1639e163e3ed66b3
sohail7250 <52400330+sohail7250@users.noreply.github.com>
2024-10-11 11:24:38 +0000

    Done

--- 05_higher_order_loops/04_filters.js
+++ 05_higher_order_loops/04_filters.js
@@ -28,15 +28,15 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 
-const newNums = []
+// const newNums = []
 
-myNums.forEach( (num) => {
-    if (num > 4) {
-        newNums.push(num)
-    }
-})
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// })
 
-console.log(newNums);
+// console.log(newNums);
 
 const books = [
     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
@@ -58,14 +58,14 @@ const books = [
     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
   ];
 
-//   let userBooks = books.filter( (bk) => bk.genre === 'History')
+  // let userBooks = books.filter( (bk) => bk.genre === 'History')
 
-//   let userBooks = books.filter( (bk) => {bk.publish >= 2000})   //  []  because we use {} so we have to use return keyword 
+  // let userBooks = books.filter( (bk) => {bk.publish >= 2000})   //  []  because we use {} so we have to use return keyword 
 
 
-//   let userBooks = books.filter( (bk) => {return bk.publish >= 2000}) 
+  // let userBooks = books.filter( (bk) => {return bk.publish >= 2000}) 
 
   let userBooks = books.filter( (bk) => {
     return bk.publish >= 1995 && bk.genre === "History"}) 
 
-  // console.log(userBooks);
+  console.log(userBooks);
