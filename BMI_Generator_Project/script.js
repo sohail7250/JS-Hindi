e0e9296a8751857b19f37a36b031c577eaa95e13
sohail7250 <52400330+sohail7250@users.noreply.github.com>
2024-10-17 07:55:17 +0000

    done

--- BMI_Generator_Project/script.js
+++ BMI_Generator_Project/script.js
@@ -19,5 +19,6 @@ form.addEventListener('submit', function (e) {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //show the result
     results.innerHTML = `<span>${bmi}</span>`;
+    
   }
 });
