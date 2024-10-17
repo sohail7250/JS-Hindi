46880c5be1ce7bec813b62864734c548624a41d2
sohail7250 <52400330+sohail7250@users.noreply.github.com>
2024-10-17 07:12:57 +0000

    color-picker-project

--- Color_Picker_Project/script.js
+++ Color_Picker_Project/script.js
@@ -6,7 +6,8 @@ buttons.forEach(function (button) {
   button.addEventListener('click', function (e) {
     console.log(e);
     console.log(e.target);
-    if (e.target.id === 'gold') {
+
+    if (e.target.id === 'orange') {
       body.style.backgroundColor = e.target.id;
     }
     if (e.target.id === 'green') {
