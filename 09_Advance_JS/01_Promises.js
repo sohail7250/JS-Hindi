b49d69d949c69518bb33ab5b7a627ace7052c0df
sohail7250 <52400330+sohail7250@users.noreply.github.com>
2024-10-18 19:24:11 +0000

    promise done

--- 09_Advance_JS/01_Promises.js
+++ 09_Advance_JS/01_Promises.js
@@ -1,81 +1,81 @@
-const promiseOne = new Promise(function(resolve, reject){
-    //Do an async task
-    // DB calls, cryptography, network
+// const promiseOne = new Promise(function(resolve, reject){
+//     //Do an async task
+//     // DB calls, cryptography, network call
 
-    setTimeout(function(){
-        console.log('Async task is compelete');
-        resolve()
-    }, 1000)
-})
+//     setTimeout(function(){
+//         console.log('Async task is compelete');
+//         resolve()
+//     }, 1000)
+// })
 
-promiseOne.then(function(){
-    console.log("Promise consumed");
-})
+// promiseOne.then(function(){
+//     console.log("Promise consumed");
+// })
 
- new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log("Async task 2");
-        resolve()
-    }, 1000)
+//  new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log("Async task 2");
+//         resolve()
+//     }, 1000)
 
-}).then(function(){
-    console.log("Async 2 resolved");
-})
+// }).then(function(){
+//     console.log("Async 2 resolved");
+// })
 
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        resolve({username: "Coffee", email: "coffee@example.com"})
-    }, 1000)
-})
+// const promiseThree = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         resolve({username: "Coffee", email: "coffee@example.com"})
+//     }, 1000)
+// })
 
-promiseThree.then(function(user){
-    console.log(user);
-})
+// promiseThree.then(function(user){
+//     console.log(user);
+// })
 
-const promiseFour = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = true
-        if (!error) {
-            resolve({username: "sohail", password: "123"})
-        } else {
-            reject('ERROR: Something went wrong')
-        }
-    }, 1000)
-})
+// const promiseFour = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = true
+//         if (!error) {
+//             resolve({username: "sohail", password: "123"})
+//         } else {
+//             reject('ERROR: Something went wrong')
+//         }
+//     }, 1000)
+// })
 
- promiseFour
- .then((user) => {
-    console.log(user);
-    return user.username
-}).then((username) => {
-    console.log(username);
-}).catch(function(error){
-    console.log(error);
-}).finally(() => console.log("The promise is either resolved or rejected"))
+//  promiseFour
+//  .then((user) => {
+//     console.log(user);
+//     return user.username
+// }).then((username) => {
+//     console.log(username);
+// }).catch(function(error){
+//     console.log(error);
+// }).finally(() => console.log("The promise is either resolved or rejected"))
 
 
 
-const promiseFive = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = true
-        if (!error) {
-            resolve({username: "javaScript", password: "123"})
-        } else {
-            reject('ERROR: JS went wrong')
-        }
-    }, 1000)
-});
+// const promiseFive = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = false
+//         if (!error) {
+//             resolve({username: "javaScript", password: "123"})
+//         } else {
+//             reject('ERROR: JS went wrong')
+//         }
+//     }, 1000)
+// });
 
-async function consumePromiseFive(){
-    try {
-        const response = await promiseFive
-        console.log(response);
-    } catch (error) {
-        console.log(error);
-    }
-}
+// async function consumePromiseFive(){
+//     try {
+//         const response = await promiseFive
+//         console.log(response);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
-consumePromiseFive()
+// consumePromiseFive()
 
 async function getAllUsers(){
     try {
